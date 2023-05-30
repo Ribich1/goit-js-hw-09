a4ea9b8982dd972406b681cf5299b263dbaba587
Ribich1 <ribich@ukr.net>
2023-05-30 23:32:42 +0200

    1

---
+++ src/js/02-timer.js
@@ -0,0 +1,77 @@
+// Описан в документации
+import flatpickr from "flatpickr";
+// Дополнительный импорт стилей
+import "flatpickr/dist/flatpickr.min.css";
+
+
+const refs = {
+  input: document.getElementById('datetime-picker'),
+  btn: document.querySelector('button'),
+  day: document.querySelector('[data-days]'),
+  hours: document.querySelector('[data-hours]'),
+  minutes: document.querySelector('[data-minutes]'),
+  seconds: document.querySelector('[data-seconds]'),
+};
+
+refs.input.addEventListener(
+    'input',
+    e => {
+        const selectedDate = new Date('e.currentTarget.value');
+        console.log("selectedDate", selectedDate);
+        
+    }
+);
+
+const currentTime = new Date();
+let selectedDate;
+refs.btn.disabled = true;
+let timer;
+
+
+
+
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+      console.dir(selectedDates[0]);
+      selectedDate = selectedDates[0];
+      if (selectedDate <= currentTime) {
+          refs.btn.disabled = true;
+          alert('Please choose a date in the future')
+      } else {
+          refs.btn.disabled = false;
+      }
+      },
+};
+
+flatpickr("#datetime-picker", options);
+
+
+
+
+
+function convertMs(ms) {
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  // Remaining days
+  const days = addLeadingZero( Math.floor(ms / day));
+  // Remaining hours
+  const hours = addLeadingZero(Math.floor((ms % day) / hour));
+  // Remaining minutes
+  const minutes = addLeadingZero(Math.floor(((ms % day) % hour) / minute));
+  // Remaining seconds
+  const seconds = addLeadingZero(Math.floor((((ms % day) % hour) % minute) / second));
+
+  return { days, hours, minutes, seconds };
+}
+
+function addLeadingZero(value) {
+  return String(value).padStart(2, '0');
+}
---
+++ src/js/flatpickr.js
@@ -0,0 +1,19 @@
+// Описан в документации
+import flatpickr from "flatpickr";
+// Дополнительный импорт стилей
+import "flatpickr/dist/flatpickr.min.css";
+
+let selDate;
+
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+      console.dir(selectedDates[0]);
+       selDate = selectedDates[0];
+      },
+};
+
+flatpickr("#datetime-picker", options);
