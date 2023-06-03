2ddc793a2a0c623d5d37b995d3763080fa49a081
Ribich1 <ribich@ukr.net>
2023-06-04 00:26:58 +0200

    09-01

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,39 @@
+const refs = {
+    btnStart: document.querySelector('button[data-start]'),
+    btnStop: document.querySelector('button[data-stop]'),
+    bodyEl: document.querySelector('body'),
+};
+
+let timerId = null;
+refs.btnStop.disabled = true;
+
+
+refs.btnStart.addEventListener('click', onhandleStart);
+refs.btnStop.addEventListener('click', onhandleStop);
+
+
+function onhandleStart() {
+    refs.btnStart.disabled = true;
+    refs.btnStop.disabled = false;
+
+timerId = setInterval(onBcgndColor, 1000);
+};
+
+
+function onhandleStop() {
+    clearInterval(timerId);
+    refs.btnStart.disabled = false;
+    refs.btnStop.disabled = true;
+    // refs.bodyEl.style.backgroundColor = '';
+}
+
+
+function onBcgndColor() {
+    refs.bodyEl.style.backgroundColor = getRandomHexColor();
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+}
+
 
